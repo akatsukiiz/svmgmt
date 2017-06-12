37d04083abad901f46518ba91e16156f818d67ab
kunanon <kunanon.kara@gmail.com>
2017-06-12 15:38:21 +0700

    add new file

--- index.php
+++ index.php
@@ -1,18 +1,46 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Server Management</title>
-	</head>
-	<body>
-		<div class="top">
-			Server Management
-		</div>
-		<div class="left-side">
-			Menu
-		</div>
-		<div class="button">
-			Contact
-		</div>
-	</body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Insert Server</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Insert server</h1>
+    <div class="row">
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+  		<div class="col-md-1">.col-md-1</div>
+	</div>
+	<div class="row">
+ 		<div class="col-md-8">.col-md-8</div>
+		<div class="col-md-4">.col-md-4</div>
+	</div>
+	<div class="row">
+  		<div class="col-md-4">.col-md-4</div>
+  		<div class="col-md-4">.col-md-4</div>
+  		<div class="col-md-4">.col-md-4</div>
+	</div>
+	<div class="row">
+  		<div class="col-md-6">.col-md-6</div>
+  		<div class="col-md-6">.col-md-6</div>
+	</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
